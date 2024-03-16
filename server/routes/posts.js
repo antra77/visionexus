782859edcc610a7c88c978aeab3a36a6d6655191
Antra Saraf <antra.saraf@gmail.com>
2024-03-16 15:11:52 +0530

    updated path

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 /* READ */
 router.get("/", verifyToken, getFeedPosts);
-router.get("/:userId/posts", verifyToken, getUserPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts); // Get user's posts by User ID
 
 /* UPDATE */
 router.patch("/:id/like", verifyToken, likePost);
